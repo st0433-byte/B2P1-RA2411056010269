c3b020653ba37f43f93baa57bab9a5a6d574ac57
st0433-byte <st0433@gmail.com>
2026-02-24 09:00:23 +0530

    UC1: Add welcome message module

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,2 +1,8 @@
-public class PalindromeCheckerApp {
+public class  PalindromeCheckerApp{
+    public static void main(String[]args){
+        System.out.println("Welcome to the palindrome chcker managemnet system");
+        System.out.println("Version : 1.0 ");
+        System.out.println("System initialized sucessfully");
+    }
 }
+
